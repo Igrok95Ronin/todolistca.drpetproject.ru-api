ff58811aa925b7e65ac1f53516367293df071a32
rivan Dilmaev <xl92@mail.ru>
2025-03-07 21:53:31 +0300

    add: .env

--- internal/config/config.go
+++ internal/config/config.go
@@ -3,6 +3,8 @@ package config
 import (
 	"fmt"
 	"github.com/ilyakaznacheev/cleanenv"
+	"github.com/joho/godotenv"
+	"os"
 	"sync"
 )
 
@@ -29,13 +31,37 @@ var once sync.Once
 
 // Функция получения конфигурации
 func GetConfig() *Config {
+
+	// Загружаем .env
+	if err := godotenv.Load(); err != nil {
+		fmt.Println("Не удалось загрузить .env файл, переменные окружения могут отсутствовать")
+	}
+
 	once.Do(func() {
 		instance = &Config{}
 
-		if err := cleanenv.ReadConfig("config.yml", instance); err != nil {
+		if err := cleanenv.ReadConfig("./config.yml", instance); err != nil {
 			help, _ := cleanenv.GetDescription(instance, nil)
 			fmt.Println(help)
 		}
+
+		// Читаем переменные окружения и заменяем значения в конфиге
+		overrideWithEnv(instance)
 	})
 	return instance
+}
+
+// overrideWithEnv перезаписывает конфигурацию значениями из переменных окружения (если они заданы)
+func overrideWithEnv(cfg *Config) {
+
+	if user := os.Getenv("POSTGRES_USER"); user != "" {
+		cfg.DB.User = user
+	}
+	if password := os.Getenv("POSTGRES_PASSWORD"); password != "" {
+		cfg.DB.Password = password
+	}
+	if dbName := os.Getenv("POSTGRES_DB"); dbName != "" {
+		cfg.DB.DBName = dbName
+	}
+
 }
